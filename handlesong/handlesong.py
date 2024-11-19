726ab453cccfb8ef60f7c3a1ca35186e677f1294
Alexandre Rodrigues <82237547+Elemento46@users.noreply.github.com>
2024-11-19 13:31:12 -0300

    Update handlesong.py

--- handlesong/handlesong.py
+++ handlesong/handlesong.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Substitua pelo seu JWT Token do StreamElements
 JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjaXRhZGVsIiwiZXhwIjoxNzQ2NjMxMTMzLCJqdGkiOiIxZGIzOWJiZS0zZjIwLTRiMzYtYTY2MC0yOTBmOWI3OTdjM2YiLCJjaGFubmVsIjoiNWY2ZmQ0ZDUxNDI0Njk4NGI3MjAxOGY0Iiwicm9sZSI6Im93bmVyIiwiYXV0aFRva2VuIjoia0dkbHg0ZGtjY2d4RVhnYTQ2cEc2V0VfVjRTTnhEbnExTHozRGVHNFF3bl9LcmdNIiwidXNlciI6IjVmNmZkNGQ1MTQyNDY5ZTJhMjIwMThmMyIsInVzZXJfaWQiOiJmMDk2NmM2Yi01Nzg5LTQ0MzItOWRjOS0wYzMxYmQwOTA3MmEiLCJ1c2VyX3JvbGUiOiJjcmVhdG9yIiwicHJvdmlkZXIiOiJ0d2l0Y2giLCJwcm92aWRlcl9pZCI6IjQ2ODc3ODkzNCIsImNoYW5uZWxfaWQiOiJhNmQ2MGVhYS1hODdiLTQzNGUtODFlZS0zYTU0YzAxNDJkZjIiLCJjcmVhdG9yX2lkIjoiMjQzMTljMmUtYTVmMy00ZDQ5LWI4ZTItM2Q4Njk4MzBhN2Y2In0.jKZsCAmrgKWAyWvE5CnTDTx-FRiRerNwn-SRubRp3AU"
 
-@app.route('/api/handlesong', methods=['GET'])
+@app.route('/handlesong/handlesong', methods=['GET'])
 def handle_song():
     try:
         action = request.args.get('action', 'approve')
